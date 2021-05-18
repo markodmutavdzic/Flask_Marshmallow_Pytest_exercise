00740c17412302ef80c1c1fd29b32fde099e7fd3
Marko <marko.mutavdzic@factoryww.com>
2021-05-18 22:59:57 +0200

    added more tests

--- test_notes.py
+++ test_notes.py
@@ -1,7 +1,9 @@
 from unittest.mock import patch
 from freezegun import freeze_time
 import pytest
-from notes import app, notes
+
+
+from notes import app
 
 
 @pytest.fixture()
@@ -33,6 +35,87 @@ def test_create_note(client):
     assert response.get_json() == expected_result
 
 
+# u narednih nekoliko testova nisam koristio freeze_time i patch uuid-a jer ionako prekida izvrsenje jer ne prolazi
+# validaciju
+def test_create_note_with_invalid_input_no_title(client):
+    data = {
+
+        "note": "Ovo je poruka",
+        "user_id": 123
+        }
+    url = '/api/note'
+    response = client.post(url, json=data)
+    assert response.status_code == 400
+
+
+def test_create_note_with_invalid_input_title_too_long(client):
+    data = {
+        "title": "Poruka 123456890123456789",
+        "note": "Ovo je poruka",
+        "user_id": 123
+        }
+    url = '/api/note'
+    response = client.post(url, json=data)
+    assert response.status_code == 400
+
+def test_create_note_with_invalid_input_no_note(client):
+    data = {
+        "title": "Poruka",
+
+        "user_id": 123
+        }
+    url = '/api/note'
+    response = client.post(url, json=data)
+    assert response.status_code == 400
+
+
+def test_create_note_with_invalid_input_note_too_long(client):
+    data = {
+        "title": "Poruka",
+        "note": "Ovo je poruka 567892123456789312345678941234567895123456789612345678971234567898123456789912345689100",
+        "user_id": 123
+        }
+    url = '/api/note'
+    response = client.post(url, json=data)
+    assert response.status_code == 400
+
+
+# ovaj test sam prvo mislio da uradim paramerizacijom pa mi je bio komplikovano, onda sam improvizovao
+def test_create_note_with_invalid_input_note_contains_forbidden_words(client):
+    forbidden_words = ['unbelievable', 'impossible', 'undoable', 'can not', 'would not']
+    for word in forbidden_words:
+        data = {
+            "title": "Poruka",
+            "note": f"Ovo je poruka{word} ",
+            "user_id": 123
+            }
+        url = '/api/note'
+        response = client.post(url, json=data)
+        assert response.status_code == 400
+
+
+def test_create_note_with_invalid_input_no_user_id(client):
+    data = {
+        "title": "Poruka",
+        "note": "Ovo je poruka",
+
+        }
+    url = '/api/note'
+    response = client.post(url, json=data)
+    assert response.status_code == 400
+
+
+def test_create_note_with_invalid_input_user_id_out_of_range(client):
+    data = {
+        "title": "Poruka",
+        "note": "Ovo je poruka",
+        "user_id": -123
+        }
+    url = '/api/note'
+    response = client.post(url, json=data)
+    assert response.status_code == 400
+
+
 def test_get_notes_with_123_note(client):
     response = client.get('/api/note')
     expected_result = {'123': {'note': 'Ovo je poruka',
@@ -42,13 +125,18 @@ def test_get_notes_with_123_note(client):
     assert response.get_json() == expected_result
 
 
-def test_get_note(client):
+def test_get_note_123(client):
     response = client.get('/api/note/123')
     expected_result = {'note': 'Ovo je poruka',
                                'time_created': '18-05-2021 15:42:50',
                                'title': 'Poruka',
                                'user_id': 123}
     assert response.get_json() == expected_result
+
+
+def test_get_note_with_non_existent_note(client):
+    response = client.get('/api/note/456')
+    assert response.status_code == 400
 
 
 @freeze_time('18-05-2021 15:50:50')
@@ -81,6 +169,50 @@ def test_update_note_second_time(client):
     assert response.get_json() == expected_result
 
 
+def test_update_note_with_invalid_input_title_too_long(client):
+    data = {
+        "title": "Poruka 123456890123456789",
+        "note": "Ovo je poruka",
+        "user_id": 123
+        }
+    url = '/api/note'
+    response = client.patch(url, json=data)
+    assert response.status_code == 405
+
+
+def test_update_note_with_invalid_input_note_too_long(client):
+    data = {
+        "title": "Poruka",
+        "note": "Ovo je poruka 567892123456789312345678941234567895123456789612345678971234567898123456789912345689100",
+        "user_id": 123
+        }
+    url = '/api/note'
+    response = client.patch(url, json=data)
+    assert response.status_code == 405
+
+def test_update_note_with_invalid_input_user_id_out_of_range(client):
+    data = {
+        "title": "Poruka",
+        "note": "Ovo je poruka",
+        "user_id": -123
+        }
+    url = '/api/note'
+    response = client.patch(url, json=data)
+    assert response.status_code == 405
+
+def test_update_note_with_invalid_input_note_contains_forbidden_words(client):
+    forbidden_words = ['unbelievable', 'impossible', 'undoable', 'can not', 'would not']
+    for word in forbidden_words:
+        data = {
+            "title": "Poruka",
+            "note": f"Ovo je poruka {word} ",
+            "user_id": 123
+            }
+        url = '/api/note'
+        response = client.patch(url, json=data)
+        assert response.status_code == 405
+
+
 def test_delete_note(client):
     response = client.delete('/api/note/123')
     expected_result = {'note': 'Ovo je druga patch poruka',
@@ -90,4 +222,7 @@ def test_delete_note(client):
                        'user_id': 123}
     assert response.get_json() == expected_result
 
+def test_delete_note_with_non_existent_note(client):
+    response = client.delete('/api/note/465')
+    assert response.status_code == 400
 
