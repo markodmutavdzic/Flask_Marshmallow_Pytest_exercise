dba694b0b0664ff9dedda253afc01afa01f16c39
Marko <marko.mutavdzic@factoryww.com>
2021-05-19 22:32:24 +0200

    updated test, dict patched

--- test_notes.py
+++ test_notes.py
@@ -1,8 +1,7 @@
 from unittest.mock import patch
 from freezegun import freeze_time
 import pytest
-
-from notes import app
+from notes_app import app
 
 
 @pytest.fixture()
@@ -11,31 +10,47 @@ def client():
     return client
 
 
-def test_get_notes_empty(client):
-    response = client.get('/api/note')
-    assert response.get_json() == {}
+@pytest.fixture()
+def notes_for_test_empty():
+    return {}
+
+
+@pytest.fixture()
+def notes_for_test_123():
+    return {'123' : {'note': 'Ovo je poruka',
+                     'time_created': '18-05-2021 15:42:50',
+                     'title': 'Poruka',
+                     'user_id': 123}}
+
+
+def test_get_notes_empty(client, notes_for_test_empty):
+    with patch.dict('notes_app.notes',notes_for_test_empty):
+        response = client.get('/api/note')
+
+        assert response.status_code == 200
+        assert response.get_json() == {}
 
 
 @freeze_time('18-05-2021 15:42:50')
-def test_create_note(client):
-    with patch("notes.uuid.uuid4") as uuid:
-        uuid.return_value = 123
-        data = {
-            "title": "Poruka",
-            "note": "Ovo je poruka",
-            "user_id": 123
-        }
-        url = '/api/note'
-        response = client.post(url, json=data)
-    expected_result = {'123': {'note': 'Ovo je poruka',
-                               'time_created': '18-05-2021 15:42:50',
-                               'title': 'Poruka',
-                               'user_id': 123}}
-    assert response.get_json() == expected_result
+def test_create_note(client, notes_for_test_empty):
+    with patch.dict('notes_app.notes', notes_for_test_empty):
+        with patch("notes_app.uuid.uuid4") as uuid:
+            uuid.return_value = 123
+            data = {
+                "title": "Poruka",
+                "note": "Ovo je poruka",
+                "user_id": 123
+            }
+            url = '/api/note'
+            response = client.post(url, json=data)
+
+        assert response.status_code == 200
+        assert response.get_json() == {'123': {'note': 'Ovo je poruka',
+                                       'time_created': '18-05-2021 15:42:50',
+                                       'title': 'Poruka',
+                                       'user_id': 123}}
 
 
-# u narednih nekoliko testova nisam koristio freeze_time i patch uuid-a jer ionako prekida izvrsenje jer ne prolazi
-# validaciju
 def test_create_note_with_invalid_input_no_title(client):
     data = {
 
@@ -44,18 +59,22 @@ def test_create_note_with_invalid_input_no_title(client):
     }
     url = '/api/note'
     response = client.post(url, json=data)
+
     assert response.status_code == 400
+    assert str(response) == '<Response streamed [400 BAD REQUEST]>'
 
 
 def test_create_note_with_invalid_input_title_too_long(client):
     data = {
-        "title": "Poruka 123456890123456789",
+        "title": "Poruka"*5,
         "note": "Ovo je poruka",
         "user_id": 123
     }
     url = '/api/note'
     response = client.post(url, json=data)
+
     assert response.status_code == 400
+    assert str(response) == '<Response streamed [400 BAD REQUEST]>'
 
 
 def test_create_note_with_invalid_input_no_note(client):
@@ -66,32 +85,48 @@ def test_create_note_with_invalid_input_no_note(client):
     }
     url = '/api/note'
     response = client.post(url, json=data)
+
     assert response.status_code == 400
+    assert str(response) == '<Response streamed [400 BAD REQUEST]>'
 
 
 def test_create_note_with_invalid_input_note_too_long(client):
     data = {
         "title": "Poruka",
-        "note": "Ovo je poruka 567892123456789312345678941234567895123456789612345678971234567898123456789912345689100",
+        "note": "Ovo je poruka"*10,
         "user_id": 123
     }
     url = '/api/note'
     response = client.post(url, json=data)
+
     assert response.status_code == 400
+    assert str(response) == '<Response streamed [400 BAD REQUEST]>'
 
 
-# ovaj test sam prvo mislio da uradim paramerizacijom pa mi je bio komplikovano, onda sam improvizovao
-def test_create_note_with_invalid_input_note_contains_forbidden_words(client):
-    forbidden_words = ['unbelievable', 'impossible', 'undoable', 'can not', 'would not']
-    for word in forbidden_words:
-        data = {
-            "title": "Poruka",
-            "note": f"Ovo je poruka{word} ",
-            "user_id": 123
-        }
-        url = '/api/note'
-        response = client.post(url, json=data)
-        assert response.status_code == 400
+def test_create_note_with_invalid_input_note_contains_forbidden_word_unbelievable(client):
+    data = {
+        "title": "Poruka",
+        "note": "Ovo je poruka unbelievable ",
+        "user_id": 123
+    }
+    url = '/api/note'
+    response = client.post(url, json=data)
+
+    assert response.status_code == 400
+    assert str(response) == '<Response streamed [400 BAD REQUEST]>'
+
+
+def test_create_note_with_invalid_input_note_contains_forbidden_word_undoable(client):
+    data = {
+        "title": "Poruka",
+        "note": "Ovo je poruka undoable ",
+        "user_id": 123
+    }
+    url = '/api/note'
+    response = client.post(url, json=data)
+
+    assert response.status_code == 400
+    assert str(response) == '<Response streamed [400 BAD REQUEST]>'
 
 
 def test_create_note_with_invalid_input_no_user_id(client):
@@ -102,7 +137,9 @@ def test_create_note_with_invalid_input_no_user_id(client):
     }
     url = '/api/note'
     response = client.post(url, json=data)
+
     assert response.status_code == 400
+    assert str(response) == '<Response streamed [400 BAD REQUEST]>'
 
 
 def test_create_note_with_invalid_input_user_id_out_of_range(client):
@@ -113,82 +150,81 @@ def test_create_note_with_invalid_input_user_id_out_of_range(client):
     }
     url = '/api/note'
     response = client.post(url, json=data)
+
     assert response.status_code == 400
+    assert str(response) == '<Response streamed [400 BAD REQUEST]>'
 
 
-def test_get_notes_with_123_note(client):
-    response = client.get('/api/note')
-    expected_result = {'123': {'note': 'Ovo je poruka',
-                               'time_created': '18-05-2021 15:42:50',
-                               'title': 'Poruka',
-                               'user_id': 123}}
-    assert response.get_json() == expected_result
+def test_get_notes_with_123_note(client, notes_for_test_123):
+    with patch.dict('notes_app.notes', notes_for_test_123):
+        response = client.get('/api/note')
+
+        assert response.status_code == 200
+        assert response.get_json() == {'123': {'note': 'Ovo je poruka',
+                                                 'time_created': '18-05-2021 15:42:50',
+                                                 'title': 'Poruka',
+                                                 'user_id': 123}}
 
 
-def test_get_note_123(client):
-    response = client.get('/api/note/123')
-    expected_result = {'note': 'Ovo je poruka',
-                       'time_created': '18-05-2021 15:42:50',
-                       'title': 'Poruka',
-                       'user_id': 123}
-    assert response.get_json() == expected_result
+def test_get_note_123(client, notes_for_test_123):
+    with patch.dict('notes_app.notes', notes_for_test_123):
+        response = client.get('/api/note/123')
+
+        assert response.status_code == 200
+        assert response.get_json() == {'note': 'Ovo je poruka',
+                                       'time_created': '18-05-2021 15:42:50',
+                                       'title': 'Poruka',
+                                       'user_id': 123}
 
 
 def test_get_note_with_non_existent_note(client):
     response = client.get('/api/note/456')
+
     assert response.status_code == 400
+    assert str(response) == '<Response streamed [400 BAD REQUEST]>'
 
 
 @freeze_time('18-05-2021 15:50:50')
-def test_update_note(client):
-    data = {
-        "note": "Ovo je patch poruka",
-    }
-    url = '/api/note/123'
-    expected_result = {'note': 'Ovo je patch poruka',
-                       'time_created': '18-05-2021 15:42:50',
-                       'time_updated': '18-05-2021 15:50:50',
-                       'title': 'Poruka',
-                       'user_id': 123}
-    response = client.patch(url, json=data)
-    assert response.get_json() == expected_result
+def test_update_note(client, notes_for_test_123):
+    with patch.dict('notes_app.notes', notes_for_test_123):
+        data = {
+            "note": "Ovo je patch poruka",
+        }
+        url = '/api/note/123'
+        response = client.patch(url, json=data)
 
-
-@freeze_time('18-05-2021 15:51:50')
-def test_update_note_second_time(client):
-    data = {
-        "note": "Ovo je druga patch poruka",
-    }
-    url = '/api/note/123'
-    expected_result = {'note': 'Ovo je druga patch poruka',
-                       'time_created': '18-05-2021 15:42:50',
-                       'time_updated': '18-05-2021 15:51:50',
-                       'title': 'Poruka',
-                       'user_id': 123}
-    response = client.patch(url, json=data)
-    assert response.get_json() == expected_result
+        assert response.status_code == 200
+        assert response.get_json() == {'note': 'Ovo je patch poruka',
+                                       'time_created': '18-05-2021 15:42:50',
+                                       'time_updated': '18-05-2021 15:50:50',
+                                       'title': 'Poruka',
+                                       'user_id': 123}
 
 
 def test_update_note_with_invalid_input_title_too_long(client):
     data = {
-        "title": "Poruka 123456890123456789",
+        "title": "Poruka "*5,
         "note": "Ovo je poruka",
         "user_id": 123
     }
-    url = '/api/note'
+    url = '/api/note/123'
     response = client.patch(url, json=data)
-    assert response.status_code == 405
+
+    assert response.status_code == 400
+    assert str(response) == '<Response streamed [400 BAD REQUEST]>'
 
 
 def test_update_note_with_invalid_input_note_too_long(client):
     data = {
         "title": "Poruka",
-        "note": "Ovo je poruka 567892123456789312345678941234567895123456789612345678971234567898123456789912345689100",
+        "note": "Ovo je poruka"*10,
         "user_id": 123
     }
-    url = '/api/note'
+    url = '/api/note/123'
     response = client.patch(url, json=data)
-    assert response.status_code == 405
+
+    assert response.status_code == 400
+    assert str(response) == '<Response streamed [400 BAD REQUEST]>'
 
 
 def test_update_note_with_invalid_input_user_id_out_of_range(client):
@@ -197,34 +233,52 @@ def test_update_note_with_invalid_input_user_id_out_of_range(client):
         "note": "Ovo je poruka",
         "user_id": -123
     }
-    url = '/api/note'
+    url = '/api/note/123'
     response = client.patch(url, json=data)
-    assert response.status_code == 405
+
+    assert response.status_code == 400
+    assert str(response) == '<Response streamed [400 BAD REQUEST]>'
 
 
-def test_update_note_with_invalid_input_note_contains_forbidden_words(client):
-    forbidden_words = ['unbelievable', 'impossible', 'undoable', 'can not', 'would not']
-    for word in forbidden_words:
-        data = {
-            "title": "Poruka",
-            "note": f"Ovo je poruka {word} ",
-            "user_id": 123
-        }
-        url = '/api/note'
-        response = client.patch(url, json=data)
-        assert response.status_code == 405
+def test_update_note_with_invalid_input_note_contains_forbidden_word_unbelievable(client):
+    data = {
+        "title": "Poruka",
+        "note": f"Ovo je poruka unbelievable ",
+        "user_id": 123
+    }
+    url = '/api/note/123'
+    response = client.patch(url, json=data)
+
+    assert response.status_code == 400
+    assert str(response) == '<Response streamed [400 BAD REQUEST]>'
 
 
-def test_delete_note(client):
-    response = client.delete('/api/note/123')
-    expected_result = {'note': 'Ovo je druga patch poruka',
-                       'time_created': '18-05-2021 15:42:50',
-                       'time_updated': '18-05-2021 15:51:50',
-                       'title': 'Poruka',
-                       'user_id': 123}
-    assert response.get_json() == expected_result
+def test_update_note_with_invalid_input_note_contains_forbidden_word_undoable(client):
+    data = {
+        "title": "Poruka",
+        "note": f"Ovo je poruka undoable ",
+        "user_id": 123
+    }
+    url = '/api/note/123'
+    response = client.patch(url, json=data)
+
+    assert response.status_code == 400
+    assert str(response) == '<Response streamed [400 BAD REQUEST]>'
+
+
+def test_delete_note(client, notes_for_test_123):
+    with patch.dict('notes_app.notes', notes_for_test_123):
+        response = client.delete('/api/note/123')
+
+        assert response.status_code == 200
+        assert response.get_json() ==  {'note': 'Ovo je poruka',
+                                       'time_created': '18-05-2021 15:42:50',
+                                       'title': 'Poruka',
+                                       'user_id': 123}
 
 
 def test_delete_note_with_non_existent_note(client):
     response = client.delete('/api/note/465')
+
     assert response.status_code == 400
+    assert str(response) == '<Response streamed [400 BAD REQUEST]>'
